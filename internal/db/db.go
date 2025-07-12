f5912da6c9bdce33bd26d0b27fe70d18685886fa
Александр Сахно <sakhno@gnivc.ru>
2025-07-12 10:57:50 +0300

    ref: service, handlers, db

---
+++ internal/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"log"
+
+	"firstproject/internal/taskService"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Указатель на объект подключения к базе данных
+var db *gorm.DB
+
+// InitDB функция подключения к базе данных
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&taskService.Task{}); err != nil {
+		log.Fatalf("Failed to migrate tasks: %v", err)
+	}
+
+	return db, nil
+}
